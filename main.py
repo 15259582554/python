2e8d94bb1f586dae546c04fe8ecdae0609eb9e22
15259582554 <162778392+15259582554@users.noreply.github.com>
2024-03-10 13:02:38 +0800

    MakeCode 项目的初始文件

---
+++ main.py
@@ -0,0 +1,9 @@
+随机数 = randint(0, 100)
+while True:
+    basic.show_string("猜对了")
+while True:
+    basic.show_string("猜错了")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,10 @@
+let 随机数 = randint(0, 100)
+while (true) {
+    basic.showString("猜对了")
+}
+while (true) {
+    basic.showString("猜错了")
+}
+basic.forever(function () {
+	
+})
